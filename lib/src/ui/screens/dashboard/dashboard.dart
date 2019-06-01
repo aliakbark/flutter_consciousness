b1a40a0d175831509b02409b317598bdf025ca91
arjunkeshav <35555485+arjunkeshav@users.noreply.github.com>
2019-06-01 14:05:35 +0530

    my collections ui added

--- lib/src/ui/screens/dashboard/dashboard.dart
+++ lib/src/ui/screens/dashboard/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_consciousness/src/ui/widgets/collection_card.dart';
 import 'package:flutter_consciousness/src/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -15,73 +16,20 @@ class _MyCollectionsState extends State<MyCollections> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              title: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ImageIcon(
-                    AssetImage(
-                      "assets/icons/home_outline.png",
-                    ),
-                    size: 24.0,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      "Flutter consciousness",
-                      style: Theme.of(context)
-                          .textTheme
-                          .title
-                          .copyWith(color: Theme.of(context).primaryColor),
-                    ),
-                  ),
-                ],
-              ),
-              floating: true,
-              snap: true,
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              elevation: 0.0,
-              pinned: false,
-              forceElevated: true,
-              actions: <Widget>[
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      "Surat",
-                      style: Theme.of(context).textTheme.body1,
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.location_on,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ];
-        },
-        body: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Text(
-                Constants.sample_text3,
-                style: Theme.of(context).textTheme.title,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
+      child: Container(
+          child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              itemCount: 4,
+              itemBuilder:
+                  (BuildContext context, int index) {
+                return GestureDetector(
+                    child:CollectionCard()
+                );
+              })
       ),
-    ));
+    ),
+    floatingActionButton: FloatingActionButton(onPressed: (){},)
+    ,);
   }
 }
 
